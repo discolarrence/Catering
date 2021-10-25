3361a18105938b033b13ef2d6c06fff6c99233be
discolarrence <discolarrence@gmail.com>
2021-10-25 18:38:16 -0400

    updated create tables

--- Catering.sql
+++ Catering.sql
@@ -3,7 +3,7 @@ CREATE DATABASE catering;
 USE catering;
 
 CREATE TABLE dbo.Vendors(
-	VendorID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	VendorID int NOT NULL PRIMARY KEY,
 	VendorName varchar(40) NOT NULL,
 	VendorURL varchar(100),
 	VendorPhone int,
@@ -11,7 +11,7 @@ CREATE TABLE dbo.Vendors(
 )
 
 CREATE TABLE dbo.Product(
-	ProductID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	ProductID int NOT NULL PRIMARY KEY,
 	ProductName varchar(40) NOT NULL,
 	VendorID int NOT NULL,
 	VendorItemCode varchar(40) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE dbo.Product(
 )
 
 CREATE TABLE dbo.Recipes(
-	RecipeID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	RecipeID int NOT NULL PRIMARY KEY,
 	RecipeName varchar(40) NOT NULL,
 	RecipeServings int NOT NULL,
 	RecipeServingQuantity int NOT NULL,
@@ -27,14 +27,14 @@ CREATE TABLE dbo.Recipes(
 )
 
 CREATE TABLE dbo.Ingredients(
-	IngredientID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	IngredientID int NOT NULL PRIMARY KEY,
 	RecipeID int NOT NULL,
 	ProductID int NOT NULL,
 	ProductQuantityOz int NOT NULL,
 )
 
 CREATE TABLE dbo.Orders(
-	OrderID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	OrderID int NOT NULL PRIMARY KEY,
 	RecipeID int NOT NULL,
 	ReadyBy datetime NOT NULL,
 	CateringType varchar(40) NOT NULL,
